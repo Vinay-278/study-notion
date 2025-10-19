87c3c447930bcd41b97c905b2f9726414b85be21
Vinay-278 <vk0565106@gmail.com>
2025-10-19 06:50:49 +0530

    second

--- Config/database.js
+++ Config/database.js
@@ -1,7 +1,11 @@
+//Import the Mongoose library to connect MongoDB with Node.js
 const mongoose= require("mongoose");
+//Load environment variables from .env file 
 require("dotenv").config();
 
+//Export a function to establish a connection to MongoDB
 exports.Connect=()=>{
+    // Connect to MongoDB using the connection string stored in the .env file
     mongoose.connect(process.env.MONGODB_URL,{})
     .then(()=>{console.log("DB Connection Successfully")})
     .catch((error)=>{
