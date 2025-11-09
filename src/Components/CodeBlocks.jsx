98c8f0614c27bdcdfdfc07711f3365873ebcb93d
Vinay-278 <vk0565106@gmail.com>
2025-11-09 14:15:09 +0530

    first

---
+++ src/Components/CodeBlocks.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import CTAButton from "../Pages/Button";
+import HighlightText from "../Pages/HighlightText";
+import { FaArrowRight } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+
+const CodeBlocks = ({
+  position,
+  heading,
+  subheading,
+  ctabtn1,
+  ctabtn2, 
+  backgroudGradient,
+  codeColor,
+  codeblock
+}) => {
+  return (
+    <div className={`flex ${position} my-20 justify-between gap-10`}>
+      {/* section1 */}
+      <div className="w-[50%] flex flex-col gap-8">
+        {heading}
+        <div className="text-[#838894] font-bold">{subheading}</div>
+        <div className="flex gap-7 mt-7">
+          <CTAButton active={ctabtn1.active} linkto={ctabtn1.linkto}>
+            <div className="flex gap-2 items-center">
+              {ctabtn1.btnText}
+              <FaArrowRight />
+            </div>
+          </CTAButton>
+          <CTAButton active={ctabtn2.active} linkto={ctabtn2.linkto}>
+            {ctabtn2.btnText}
+          </CTAButton>
+        </div>
+      </div>
+      {/* Section2 */}
+      <div className="h-fit flex flex-row text-[12px] py-3 sm:text-sm leading-[18px] sm:leading-6 relative w-full lg:w-[470px]">
+        <div className="text-center flex flex-col w-[10%] text-[#6E727F] font-bold">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+        </div>
+        <div
+          className={`w-[90%] flex flex-col font-bold gap-2 ${codeColor} pr-1`}
+        >
+          <TypeAnimation
+            sequence={[codeblock, 500, ""]}
+            repeat={Infinity}
+            cursor={true}
+            style={{
+              whiteSpace: "pre-line",
+              display: "block",
+            }}
+            omitDeletionAnimation={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodeBlocks;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { FaArrowRight } from 'react-icons/fa'
-import { Link } from 'react-router'
-import HighlightText from './HighlightText';
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router";
+import HighlightText from "./HighlightText";
+import CTAButton from "./Button";
+import Banner from "../assets/Images/banner1.mp4";
+import CodeBlocks from "../Components/CodeBlocks";
 
 const Home = () => {
   return (
     <div>
       {/* section 1 */}
-      <div className="relative mx-auto flex flex-col w-11/12 items-center justify-between text-white">
+      <div className="relative mx-auto flex flex-col w-11/12 items-center justify-between gap-8 text-white">
         <Link to={"/signup"}>
           <div className="group mt-16 p-1 mx-auto rounded-full bg-[#161D29] font-bold transition-all duration-200 hover:scale-95 w-fit">
             <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-[#000814]">
@@ -16,9 +19,80 @@ const Home = () => {
             </div>
           </div>
         </Link>
-        <div className='text-center text-4xl font-semibold mt-7'>
-          Empower Your Future with 
-          <HighlightText text={" Coding Skills"}/>
+        <div className="text-center text-4xl font-semibold mt-7">
+          Empower Your Future with
+          <HighlightText text={" Coding Skills"} />
+        </div>
+        <div className="mt-4 w-[90%] text-center text-lg font-bold text-[#838894]">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+        <div className="flex flex-row gap-7 mt-8">
+          <CTAButton active={true} linkto={"/signup"}>
+            Learn More
+          </CTAButton>
+          <CTAButton active={false} linkto={"/login"}>
+            Book a Demo
+          </CTAButton>
+        </div>
+        <div className=" mx-3 my-7 shadow-[10px_-5px_40px_-5px_aqua]">
+          <video src={Banner} loop muted autoPlay></video>
+        </div>
+        {/* codeSection1 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row"}
+            heading={
+              <div className="w-full text-4xl font-semibold lg:w-[50%]">
+                Unlock your
+                <HighlightText text={" coding potential "} />
+                with our online courses.
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it Yourself",
+              link: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              link: "/signup",
+              active: false,
+            }}
+            codeColor={"text-[aqua]"}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            backgroudGradient={<div className="codeblock1 absolute"></div>}
+          />
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="w-full text-4xl font-semibold lg:w-[50%]">
+                Start
+                <HighlightText text={" coding in seconds "} />
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it Yourself",
+              link: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              link: "/signup",
+              active: false,
+            }}
+            codeColor={"text-[aqua]"}
+            codeblock={`import numpy as np\nimport pandas as pd\nimport matplotlib.pyplot as plt\nimport seaborn as sns\nfrom sklearn.linear_model import LogisticRegression\n\nX = np.array([1,2,3,4,5])\nY = np.array([1,2,2,2,1])\n\nmodel = LogisticRegression()\nmodel.fit(X,Y)`}
+            backgroudGradient={<div className="codeblock1 absolute"></div>}
+          />
         </div>
       </div>
 
@@ -29,6 +103,6 @@ const Home = () => {
       {/* Footer */}
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
